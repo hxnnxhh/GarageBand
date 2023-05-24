330a3e16f8ed6713a10da73c633a56fca5411da3
HannahR <wasntherebye@gmail.com>
2023-05-24 13:52:25 -0700

    buttons

--- app/src/main/java/com/example/garageband/MainActivity.java
+++ app/src/main/java/com/example/garageband/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Buttons to hit
+
+    private Button btnBass
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
